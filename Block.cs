0e1a8c09b2368df3cbea9c9457a2e6968925fa0d
maldron0309 <bagjinhyeong640@gmail.com>
2023-12-17 21:24:07 +0900

    [add] Block

--- Block.cs
+++ Block.cs
@@ -6,11 +6,78 @@ using System.Threading.Tasks;
 
 namespace Tetris
 {
-    internal abstract class Block
+    public abstract class Block
     {
         protected abstract Position[][] Tiles { get; }
-        protected abstract Position[][] StartOffset { get; }
+        protected abstract Position StartOffset { get; }
 
         public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        /// <summary>
+        /// 'Block' 클래스의 기본 생성자입니다. 'offset'을 'StartOffset'으로 초기화합니다.
+        /// </summary>
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        /// <summary>
+        /// 현재 블록의 모든 타일 위치를 열거합니다.
+        /// </summary>
+        /// <returns>현재 회전 상태와 위치 오프셋을 고려한 타일 위치의 열거자를 반환합니다.</returns>
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        /// <summary>
+        /// 블록을 시계 방향으로 회전시킵니다.
+        /// </summary>
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        /// <summary>
+        /// 블록을 반시계 방향으로 회전시킵니다.
+        /// </summary>
+        public void RotateCCW()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+
+        /// <summary>
+        /// 블록을 주어진 행과 열 만큼 이동시킵니다.
+        /// </summary>
+        /// <param name="rows">이동할 행의 수입니다.</param>
+        /// <param name="columns">이동할 열의 수입니다.</param>
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        /// <summary>
+        /// 블록의 상태를 초기화합니다. 회전 상태를 0으로 되돌리고, 위치를 'StartOffset'으로 재설정합니다.
+        /// </summary>
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
     }
 }
--- Position.cs
+++ Position.cs
@@ -10,7 +10,7 @@ namespace Tetris
     /// 'Position' 클래스는 테트리스 게임 그리드에서의 위치를 표현합니다.
     /// 이 클래스는 내부적으로만 사용되며, 동일한 어셈블리 내에서만 접근이 가능합니다.
     /// </summary>
-    internal class Position
+    public class Position
     {
         /// <summary>
         /// 위치의 행을 나타내는 속성입니다.
@@ -21,6 +21,7 @@ namespace Tetris
         /// 위치의 열을 나타내는 속성입니다.
         /// </summary>
         public int Column { get; set; }
+        public int Coloumn { get; internal set; }
 
         /// <summary>
         /// 'Position' 클래스의 생성자입니다. 행과 열의 위치를 인자로 받아 위치를 초기화합니다.
